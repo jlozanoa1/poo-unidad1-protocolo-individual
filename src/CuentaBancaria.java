623d787818530906572df03d7f0232fab6f75bb4
Juan Lozano <jlozanoa1@unicartagena.edu.co>
2026-08-30 16:27:16 -0500

    Creado el constructor base con sus validaciones en la clase "CuentaBancaria".

--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -1,7 +1,22 @@
 class CuentaBancaria {
+    private String numeroCuenta;
+    private double saldo;
+    private String tipoCuenta;
+
     // ---------- CONSTRUCTORES ----------
+    // 1. Constructor principal.
     public CuentaBancaria(String numeroCuenta, double saldo, String tipoCuenta) {
-        // CONSTRUCTOR BASE
+        if (numeroCuenta.length() != 10) {
+            throw new IllegalArgumentException("El número de cuenta debe tener 10 dígitos.");
+        }
+
+        if (saldo < 1) {
+            throw new IllegalArgumentException("El saldo no puede ser menor que $1.");
+        }
+
+        if (tipoCuenta.toLowerCase().equals("credito") || tipoCuenta.toLowerCase().equals("debito")) {
+            throw new IllegalArgumentException("El tipo de cuenta debe ser \"credito\" o \"debito\".");
+        }
     }
     // ---------- FIN - CONSTRUCTORES ----------
 }
