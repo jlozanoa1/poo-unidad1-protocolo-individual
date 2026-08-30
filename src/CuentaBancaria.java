6c5579495babc2882749e3887b6be6a20c8139ba
Juan Lozano <jlozanoa1@unicartagena.edu.co>
2026-08-30 16:16:12 -0500

    Creada la clase "CuentaBancaria" dentro de CuentaBancaria.java (clase vacía).

---
+++ src/CuentaBancaria.java
@@ -0,0 +1,7 @@
+class CuentaBancaria {
+    // ---------- CONSTRUCTORES ----------
+    public CuentaBancaria(String numeroCuenta, double saldo, String tipoCuenta) {
+        // CONSTRUCTOR BASE
+    }
+    // ---------- FIN - CONSTRUCTORES ----------
+}
