f1699ee72a21116861ccb38594a28b0bde03eec3
Juan Lozano <jlozanoa1@unicartagena.edu.co>
2026-08-30 19:22:22 -0500

    Agregadas validaciones para el constructor principal en Estudiante.java.

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -3,7 +3,23 @@ public class Estudiante {
     private int edad;
     private String curso;
 
-    public Estudiante() {
+    // ------------- CONSTRUCTORES -------------
+    // 1. Constructor principal
+    public Estudiante(String nombre, int edad, String curso) {
+        if ((nombre != null && nombre.isEmpty()) || (nombre != null && nombre.isBlank())) {
+            throw new IllegalArgumentException("El nombre del estudiante no debe estar vacío.");
+        }
 
+        if (edad < 1) {
+            throw new IllegalArgumentException("La edad del estudiante no debe ser menor a 0.");
+        }
+
+        if ((curso != null && curso.isEmpty()) || (curso != null && curso.isBlank())) {
+            throw new IllegalArgumentException("El curso del estudiante no debe estar vacío.");
+        }
+
+        this.nombre = nombre;
+        this.edad = edad;
+        this.curso = curso;
     }
 }
